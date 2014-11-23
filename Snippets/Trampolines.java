7d507baf73d4fb10a9b71f3df8db3b0a57be3d53
jesus997 <jesus9711@live.com.mx>
2014-11-23 17:49:46 -0600

    Update Trampolines.java

--- Snippets/Trampolines.java
+++ Snippets/Trampolines.java
@@ -11,20 +11,20 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class main extends JavaPlugin{
+public class main extends JavaPlugin implements Listener{
 
 	public void onEnable(){
 		PluginManager pm = Bukkit.getServer().getPluginManager();
 		pm.registerEvents(this, this);
 	}
 	
-  @EventHandler
-    public void onTrampoline(PlayerMoveEvent event){
-        Player p = event.getPlayer();
-        Vector v = event.getPlayer().getLocation().getDirection();
-        if(p.getLocation().subtract(0, 1, 0).getBlock().getType() == Material.GOLD_PLATE){
-            p.setVelocity(new Vector(v.multiply(2));
-            // p.setVelocity(new Vector(p.getVelocity().getX(), 2, p.getVelocity().getZ()));
+	@EventHandler
+	public void onTrampoline(PlayerMoveEvent event){
+		Player p = event.getPlayer();
+        	Vector v = event.getPlayer().getLocation().getDirection();
+        	if(p.getLocation().subtract(0, 1, 0).getBlock().getType() == Material.GOLD_PLATE){
+        		p.setVelocity(new Vector(v.multiply(2));
+        		// p.setVelocity(new Vector(p.getVelocity().getX(), 2, p.getVelocity().getZ()));
         }
     }
 }
